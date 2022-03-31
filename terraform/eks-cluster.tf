9548c78eb7eb1bfa9392c74b1a8a11414237bb0b
Valentyn Chubukin <val@valch.name>
2022-03-31 20:38:52 +0200

    change instance type

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -14,14 +14,14 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t3.small"
+      instance_type                 = "t3.nano"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 1
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t3.small"
+      instance_type                 = "t3.nano"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
